cf50cf943f4d610b09133a2c43772273fde2dc11
DESKTOP-EF8RDN9\User <anton.lyhin@innowise.com>
2026-02-13 15:52:11 +0100

    turn off hash in filename;

--- ReactApp/vite.config.ts
+++ ReactApp/vite.config.ts
@@ -36,5 +36,12 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, '../wwwroot/libs'),
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
   },
 });
