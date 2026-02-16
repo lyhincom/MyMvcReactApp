63e99b08dedca4f37a6d4f78bde12e8cf358fb95
DESKTOP-EF8RDN9\User <anton.lyhin@innowise.com>
2026-02-16 09:30:55 +0100

    sign in page, init commit;

--- ReactApp/src/pages/new-pages/sign-in-demo-page.tsx
+++ ReactApp/src/pages/new-pages/sign-in-demo-page.tsx
@@ -18,21 +18,48 @@ export default function Page() {
       />
       <meta name="keywords" content="Lesson 1" />
 
-      <DashboardContent>
-        <Typography variant="h4" sx={{ mb: { xs: 3, md: 5 } }}>
-          Hi, Sign In Demo Page 👋
-        </Typography>
+      <Box
+        sx={{
+          position: 'relative',
+          minHeight: '100vh',
+          '&::before': {
+            zIndex: 1,
+            opacity: 0.24,
+            width: '100%',
+            height: '100%',
+            content: "''",
+            position: 'absolute',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center center',
+            backgroundImage: 'url(/assets/background/overlay.jpg)',
+          },
+        }}
+      >
+        <DashboardContent>
+          <Typography variant="h4" sx={{ mb: { xs: 3, md: 5 } }}>
+            Hi, Sign In Demo Page 👋
+          </Typography>
 
-        <Box
-          sx={{
-            width: 1,
-            maxWidth: '420px',
-            mx: 'auto',
-          }}
-        >
-          <SignInPage />
-        </Box>
-      </DashboardContent>
+          <Box
+            sx={(theme) => ({
+              py: 5,
+              px: 3,
+              width: 1,
+              zIndex: 2,
+              borderRadius: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              maxWidth: '420px',
+              mx: 'auto',
+              position: 'relative',
+              bgcolor: theme.vars.palette.background.default,
+            })}
+          >
+            <SignInPage />
+          </Box>
+        </DashboardContent>
+      </Box>
     </>
   );
 }
